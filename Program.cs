0442a3d500d3bbfeb0706cd10426995875f68351
JesusLinares01 <34040095+JesusLinares01@users.noreply.github.com>
2017-12-14 10:54:06 -0600

    agregue el archivo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NumbersWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Words = "";
+            Words = Console.ReadLine();
+            int count = 1;
+
+
+            for (int i = 0; i < Words.Length; i++)
+            {
+
+                if (Words[i] >= 'A' && Words[i] <= 'Z')
+                {
+
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+
+
+
+        }
+    }
+}
